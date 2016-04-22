cbd4e5a552195b89a90724adc97a166bf5eacdf4
Ricardo Biehl Pasquali <rbpoficial@gmail.com>
2016-04-22 15:45:20 -0300

    Corrected BUGS in lion-mount.sh script * Added "#!/bin/bash" in the first line. * Added verification if "./lionfs" binary is a normal file (to prevent directories).

--- lion-mount.sh
+++ lion-mount.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # lionfs, The Link Over Network File System
 # Copyright (C) 2016  Ricardo Biehl Pasquali <rbpoficial@gmail.com>
 #
@@ -15,8 +17,8 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-if [ ! -x ./lionfs ]; then
-	echo "No lionfs executable binary found!"
+if [ ! -f ./lionfs -a -x ./lionfs ]; then
+	echo "No ./lionfs executable binary found!"
 	exit 1
 fi
 
